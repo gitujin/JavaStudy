07501992ae76555f2284ab9821af27257f5bd81d
gitujin <syj991114@naver.com>
2022-01-16 22:21:36 +0900

    second commit

---
+++ Chapter13/src/innerclass/InnerTest.java
@@ -0,0 +1,65 @@
+package innerclass;
+
+class OutClass{
+	private int num = 10;
+	private static int sNum = 20;
+	private InClass inClass;
+	
+	static class InStaticClass{
+		int inNum = 100;
+		static int sInNum = 200;
+		
+		void inTest() {
+			System.out.println("InStaticCalss inNum = " + inNum + "(내부 클래스의 인스턴스 변수 사용)");
+			System.out.println("InStaticCalss sinNum = " + sInNum + "(내부 클래스의 정적 변수 사용)");
+			System.out.println("InStaticCalss inNum = " + sNum + "(외부 클래스의 정적 변수 사용)");
+		}
+		
+		static void sTest() {
+			//num += 10;
+			//inNum += 10;
+			// 외부 클래스와 내부 클래스의 인스턴스 변수는 사용할 수 없으므로 주석 처리
+			
+			System.out.println("OutClass sNum = " + sNum + "(외부 클래스의 정적 변수 사용)");
+			System.out.println("InStaticClass sInNum = " + sInNum + "(내부 클래스의 정적 변수 사용)");
+		}
+	}
+
+	public OutClass() {
+		inClass = new InClass();
+	}
+	
+	class InClass{
+		int inNum = 100;
+		//static int sInNum = 200. 인스턴스 내부 클래스에 정적 변수 선언 불가능. 오류가 발생하므로 주석 처리함.
+		
+		void inTest() {
+			System.out.println("OutClass num = " + num + "(외부 클래스의 인스턴스 변수)");
+			System.out.println("OutClass sNum = " + sNum + "(외부 클래스의 정적 변수)");			
+			System.out.println();
+		}
+		//static void sTest(){
+		//} 정적 메서드 역시 정의 불가능. 오류가 발생하므로 주석 처리
+	}
+	public void usingClass() {
+		inClass.inTest();
+	}
+}
+
+public class InnerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OutClass outClass = new OutClass();
+		System.out.println("외부 클래스 이용하여 내부 클래스 기능 호출");
+		outClass.usingClass();
+		
+		OutClass.InStaticClass sInClass = new OutClass.InStaticClass();
+		System.out.println("정적 내부 클래스 일반 메서드 호출");
+		sInClass.inTest();
+		System.out.println();
+		System.out.println("정적 내부 클래스의 정적 메서드 호출");
+		OutClass.InStaticClass.sTest();
+	}
+
+}
