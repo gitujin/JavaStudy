da0a5ef93bd883bb57e6b62b46301c3c156fa31c
gitujin <syj991114@naver.com>
2022-01-18 22:51:16 +0900

    4 commit

---
+++ Alone/src/chapter12/arrayList/MemberArrayList.java
@@ -0,0 +1,48 @@
+package chapter12.arrayList;
+
+import java.util.ArrayList;
+
+import chapter12.Member;
+
+public class MemberArrayList {
+	private ArrayList<Member> arrayList;
+
+	public MemberArrayList() {
+		arrayList = new ArrayList<Member>();
+	}
+	
+	public void addMember(Member member) {
+		arrayList.add(member);
+	}
+	
+	public void insertMember(Member member, int position) {
+		if(position < 0 || position > arrayList.size()+1) {
+			System.out.println("지정 된 자리에 추가할 수 없습니다.");
+			return;
+		}
+		arrayList.add(position-1, member);
+	}
+	
+	public boolean removeMember(int memberId) {
+		
+		for(int i = 0; i < arrayList.size(); i++) {
+			Member member = arrayList.get(i);
+			int tempId = member.getMemberId();
+			if(memberId == tempId){
+			arrayList.remove(i);
+			return true;
+			}
+		}
+		
+		System.out.println(memberId + "가 존재하지 않습니다.");
+		return false;
+		}
+	
+	public void showAllMember() {
+		for(Member member : arrayList) {
+			System.out.println(member);
+		}
+		System.out.println(arrayList);
+		System.out.println();
+	}
+}
